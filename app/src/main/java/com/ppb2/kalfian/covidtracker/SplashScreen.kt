509040aef1da7b75504d9a73a68c933a0afed2d8
Kukuh Alfian <kukuh@qiscus.co>
2021-11-28 01:06:47 +0700

    Fix dependencies

---
+++ app/src/main/java/com/ppb2/kalfian/covidtracker/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.ppb2.kalfian.covidtracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    private val SPLASH_TIME: Long = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+        // TODO: Create logic to handle auto login
+        }, SPLASH_TIME)
+    }
+}
