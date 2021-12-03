3686f95193fb3c7bbdaf493404b31fff01d57b09
Kukuh Alfian <kukuh@qiscus.co>
2021-12-03 22:07:37 +0700

    Implement Splash Screen

--- app/src/main/java/com/ppb2/kalfian/covidtracker/SplashScreen.kt
+++ app/src/main/java/com/ppb2/kalfian/covidtracker/SplashScreen.kt
@@ -1,17 +1,24 @@
 package com.ppb2.kalfian.covidtracker
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.ppb2.kalfian.covidtracker.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
     private val SPLASH_TIME: Long = 1000
+    private lateinit var b: ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        b = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val v = b.root
+        setContentView(v)
+        
+        b.rotateloading.start()
 
         Handler(Looper.getMainLooper()).postDelayed({
         // TODO: Create logic to handle auto login
